e2ac86857b0fdbba60cac75aa1787ce49a73835b
Scott Ellis <scottellis94@gmail.com>
2023-06-12 23:57:01 -0400

    EOD commit

--- backend/recipeApi/src/main/java/recipeapi/models/recipe/RecipeListConverter.java
+++ backend/recipeApi/src/main/java/recipeapi/models/recipe/RecipeListConverter.java
@@ -1,14 +1,16 @@
 package recipeapi.models.recipe;
 
 import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Converter
 public class RecipeListConverter implements AttributeConverter<List<String>, String> {
     @Override
     public String convertToDatabaseColumn(List<String> strings) {
-        return String.join(";",strings);
+        return String.join(";",strings.stream().map(String::trim).toList());
     }
 
     @Override
